c4c57e65736c1eaacdebcc9cb7f7a9887d4a5a1b
Brennan Duck <brennantduck@gmail.com>
2025-08-26 19:06:41 -0400

    oof so slow

--- main.go
+++ main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+const NUM_ITERATIONS int = 10000000
 
 func main() {
-	fmt.Println("test")
+	primeCount := 0
+	start := time.Now()
+	countPrimes(&primeCount, NUM_ITERATIONS)
+	fmt.Println("there are ", primeCount, " prime numbers.")
+	fmt.Println("it took ", time.Since(start), " without concurrency")
+
+	jobs := make(chan int)
+	res := make(chan bool)
+
+	primeCount = 0
+	start = time.Now()
+	go func() {
+		for i := 0; i < NUM_ITERATIONS; i++ {
+			jobs <- i
+		}
+		close(jobs)
+	}()
+
+	var wg sync.WaitGroup
+	W := runtime.NumCPU()
+	wg.Add(W)
+	for i := 0; i < W; i++ {
+		go worker(jobs, res, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(res)
+	}()
+
+	for b := range res {
+		if b {
+			primeCount++
+		}
+	}
+
+	fmt.Println("there are ", primeCount, " prime numbers.")
+	fmt.Println("it took ", time.Since(start), " with concurrency")
+}
+
+func worker(jobs <-chan int, res chan<- bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for n := range jobs {
+		b := isPrime(n)
+		res <- b
+	}
+}
+
+func countPrimes(primeCount *int, n int) {
+	for i := 0; i <= n; i++ {
+		if isPrime(i) {
+			*primeCount++
+		}
+	}
+}
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
